5df44319771a3441a74477f75d39c8dc1a876a90
Mayko2003 <maykox34f@gmail.com>
2022-11-22 20:19:28 -0300

    chore: config database connection

--- db.js
+++ db.js
@@ -1,1 +1,12 @@
-console.log('Hello world');
+const mongoose = require('mongoose');
+
+const url = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
+mongoose.connect(url).then(async () => {
+    console.log('Connected to Wikachu database');
+}).catch(err => {
+    console.log('Error connecting to Wikachu database\n', err);
+});
+
+
+module.exports = mongoose;
