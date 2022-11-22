bee77e1d0585535c31424f8bcb7aaa48cd38532a
Mayko2003 <maykox34f@gmail.com>
2022-11-22 20:20:19 -0300

    chore: config server

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+const { mongoose } = require('./db');
+const express = require('express');
+const cors = require('cors');
+
+
+
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+//routes
+
+
+//configs
+app.set('port', process.env.PORT || 3000);
+
+//start server
+app.listen(app.get('port'), () => {
+    console.log('Server started on port', app.get('port'));
+});
+
