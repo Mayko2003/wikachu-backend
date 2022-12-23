ef73def5febc0650b6d8c6b315bc41a558ac6a4b
Mayko2003 <maykox34f@gmail.com>
2022-12-23 00:12:17 -0300

    chore: modify routes and imports

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,16 +1,17 @@
 
-const userController = require('../controllers/userController')
-const authController = require('../controllers/authController')
+const  { userController, authController } = require('../controllers')
 const express = require('express')
 
 const router = express.Router()
 
 router.get('/', authController.verifyToken, userController.getUsers)
-router.post('/create', authController.verifyToken, userController.createUser)
+router.post('/create', userController.createUser)
 router.delete('/delete/:id', authController.verifyToken, userController.deleteUser)
 router.put('/update/:id', authController.verifyToken, userController.updateUser)
 router.get('/:id', authController.verifyToken, userController.getUserById)
 router.post('/login', userController.loginUser)
+
+
 router.get('/logged', authController.verifyToken, userController.getLoggedUser)
 
 module.exports = router
