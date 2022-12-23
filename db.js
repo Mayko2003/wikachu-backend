d123ae9828c91655e0eeee69c4b0bff9e79b53eb
Mayko2003 <maykox34f@gmail.com>
2022-12-23 00:06:54 -0300

    chore: use init db script

--- db.js
+++ db.js
@@ -4,6 +4,11 @@ const url = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.e
 
 mongoose.connect(url).then(async () => {
     console.log('Connected to Wikachu database');
+    if(process.env.IS_DB_INIT === 'false') {
+        console.log('Initializing Wikachu database')
+        const initDB = require('./initdb');
+        await initDB();
+    }
 }).catch(err => {
     console.log('Error connecting to Wikachu database\n', err);
 });
