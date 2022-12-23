ef68c92959ee02647eecb4a03897383159474d77
Mayko2003 <maykox34f@gmail.com>
2022-12-23 00:10:40 -0300

    chore: create bundle file for controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,12 @@
-console.log('Hello world');
+
+const pokemonController = require('./pokemon.controller');
+const userController = require('./user.controller');
+const authController = require('./auth.controller');
+const favouriteController = require('./favourite.controller');
+
+module.exports = {
+    pokemonController,
+    userController,
+    authController,
+    favouriteController
+}
