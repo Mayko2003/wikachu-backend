502e63d2726d3f4093f42de36b6c994ba6e3612a
Mayko2003 <maykox34f@gmail.com>
2022-12-23 00:10:49 -0300

    chore: add pokemon controller

---
+++ controllers/pokemon.controller.js
@@ -0,0 +1,63 @@
+const { Pokemon } = require('../models');
+
+const pokemonController = {}
+
+
+
+pokemonController.getPokemons = async (req, res) => {
+    try {
+        const pokemons = await Pokemon.find()
+        res.status(200).json({
+            data: pokemons
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error
+        })
+    }
+}
+
+pokemonController.getPopularPokemons = async (req, res) => {
+    try {
+        const pokemons = await Pokemon.find().sort({ likes: -1 }).limit(3)
+        res.status(200).json({
+            data: pokemons
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error
+        })
+    }
+}
+
+pokemonController.getGenerationPokemons = async (req, res) => {
+    try {
+        const pokemons = await Pokemon.find({ generation: req.params.generation })
+        res.status(200).json({
+            data: pokemons
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error
+        })
+    }
+}
+
+pokemonController.updatePokemon = async (req, res) => {
+    try {
+        const id = req.params.id
+        await Pokemon.findOneAndUpdate({ _id: id }, req.body)
+        res.status(200).json({
+            message: 'Pokemon updated'
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error
+        })
+    }
+}
+
+
+
+
+module.exports = pokemonController
