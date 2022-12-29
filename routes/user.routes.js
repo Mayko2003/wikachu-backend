47b42f9dea979c7709fc48d7b02b1d03167fcc61
Mayko2003 <maykox34f@gmail.com>
2022-12-29 12:42:04 -0300

    fix: change path for get user by id

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -8,7 +8,7 @@ router.get('/', authController.verifyToken, userController.getUsers)
 router.post('/create', userController.createUser)
 router.delete('/delete/:id', authController.verifyToken, userController.deleteUser)
 router.put('/update/:id', authController.verifyToken, userController.updateUser)
-router.get('/:id', authController.verifyToken, userController.getUserById)
+router.get('/find/:id', authController.verifyToken, userController.getUserById)
 router.post('/login', userController.loginUser)
 
 
