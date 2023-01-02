426a55a542441ace7dc7fb6c37b5f6bf4421c64c
Mayko2003 <maykox34f@gmail.com>
2023-01-02 01:36:17 -0300

    chore: add script for generate json data

---
+++ initData.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const POKE_API_URL = 'https://pokeapi.co/api/v2';
+
+const endpoints = [
+    'pokemon-species',
+    'item',
+]
+
+const fetchData = async (url) => {
+    const response = await fetch(url);
+    const { results } = await response.json();
+    return results;
+}
+
+const generateData = (endpoint) => {
+    fetchData(`${POKE_API_URL}/${endpoint}?limit=5000`).then((data) => {
+        fs.writeFileSync(`./data/${endpoint}.json`, JSON.stringify(data));
+    })
+}
+
+endpoints.forEach(endpoint => {
+    generateData(endpoint);
+})
