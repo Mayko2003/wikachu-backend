863efbe605f177ad75e534924e4a4f1ecad16282
Mayko2003 <maykox34f@gmail.com>
2023-01-02 01:37:05 -0300

    chore: use refPath for use any model to like

--- models/favourite.js
+++ models/favourite.js
@@ -3,8 +3,13 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const FavouriteSchema = new Schema({
-    pokemon: { type: Schema.Types.ObjectId, ref: 'Pokemon' },
+    obj: { type: Schema.Types.ObjectId, refPath: 'onModel' },
     user: { type: Schema.Types.ObjectId, ref: 'User' },
+    onModel: {
+        type: String,
+        required: true,
+        enum: ['Pokemon', 'Ability', 'Move', 'Item', 'Type']
+    }
 })
 
 module.exports = mongoose.models.Favourite || mongoose.model('Favourite', FavouriteSchema)
