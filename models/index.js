e38cb211c44361f6b70166f68989abf6bb394f36
Mayko2003 <maykox34f@gmail.com>
2023-01-02 01:37:17 -0300

    chore: add Item model to exports

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 const Pokemon = require('./pokemon');
 const User = require('./user');
 const Favourite = require('./favourite');
+const Item = require('./item')
 
 module.exports = {
     Pokemon,
     User,
-    Favourite
+    Favourite,
+    Item
 }
