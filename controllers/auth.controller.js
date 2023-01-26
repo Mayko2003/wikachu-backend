85741a025180224c879ed7fba96e3721a3a8334c
luisrodri45 <luisedurodriguez2014@gmail.com>
2023-01-26 13:15:23 -0300

    chore: add verifyTokenFirebase

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,34 +1,74 @@
 const jwt = require('jsonwebtoken')
+const admin = require('../firebaseConfig');
 const authController = {}
 
 authController.verifyToken = async (req, res, next) => {
 
-    if(!req.headers.authorization){
+    if (!req.headers.authorization) {
         res.status(401).json({
             message: 'Unauthorized request'
         })
     }
-    else{
+    else {
         const header = req.headers.authorization.split(' ')
         var token = null
-        if(header.length === 2) token = header[1]
-        
-        if(!token){
+        if (header.length === 2) token = header[1]
+
+        if (!token) {
             res.status(401).json({
                 message: 'Unauthorized request'
             })
         }
-        else{
-            try{
+        else {
+            try {
                 const payload = jwt.verify(token, process.env.JWT_SECRET)
                 //console.log(payload._id)
                 next()
             }
-            catch(err){
-                res.status(401).json({
-                    message: 'Unauthorized request'
-                })
+            catch (err) {
+                try {
+                    admin.auth().verifyIdToken(token)
+                        .then((decodedToken) => {
+                            next();
+                        })
+                }
+                catch (err) {
+                    res.status(401).json({
+                        message: 'Unauthorized request'
+                    })
+                }
             }
+        }
+    }
+}
+
+authController.verifyTokenFirebase = async (req, res, next) => {
+
+    if (!req.headers.authorization) {
+        res.status(401).json({
+            message: 'Unauthorized request'
+        })
+    }
+    else {
+        const header = req.headers.authorization.split(' ')
+        var token = null
+        if (header.length === 2) token = header[1]
+
+        if (!token) {
+            res.status(401).json({
+                message: 'Unauthorized request'
+            })
+        }
+        else {
+            admin.auth().verifyIdToken(token)
+                .then((decodedToken) => {
+                    next();
+                })
+                .catch((err) => {
+                    res.status(401).json({
+                        message: 'Unauthorized request'
+                    })
+                })
         }
     }
 }
