8b2487de055037b7f32211392f8726341566113b
Mayko2003 <maykox34f@gmail.com>
2023-01-26 20:12:53 -0300

    chore: change username by display name

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose; 
 
 const userSchema = new Schema({
-    username: { type: String, required: true, unique: true },
+    displayName: { type: String, required: true },
     password: { type: String, required: true },
     state: { type: Boolean, default: true },
     email: { type: String, required: true, unique: true },
