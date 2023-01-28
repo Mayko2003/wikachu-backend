661c3a2eda47841927beb63b49c954503c2a3813
Mayko2003 <maykox34f@gmail.com>
2023-01-28 19:25:29 -0300

    chore: export only connection for use

--- db.js
+++ db.js
@@ -1,16 +1,4 @@
 const mongoose = require('mongoose');
 const url = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
-mongoose.connect(url).then(async () => {
-    console.log('Connected to Wikachu database');
-    if(process.env.IS_DB_INIT === 'false') {
-        console.log('Initializing Wikachu database')
-        const initDB = require('./utils/initdb');
-        await initDB();
-    }
-}).catch(err => {
-    console.log('Error connecting to Wikachu database\n', err);
-});
-
-
-module.exports = mongoose;
+module.exports = mongoose.connect(url);
