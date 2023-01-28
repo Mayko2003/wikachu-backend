aa100424bcaa42bfe0495c61dea0c64fc92a3453
Mayko2003 <maykox34f@gmail.com>
2023-01-28 19:27:10 -0300

    chore: add move and nature models to exports

--- models/index.js
+++ models/index.js
@@ -2,10 +2,14 @@ const Pokemon = require('./pokemon');
 const User = require('./user');
 const Favourite = require('./favourite');
 const Item = require('./item')
+const Move = require('./move')
+const Nature = require('./nature')
 
 module.exports = {
     Pokemon,
     User,
     Favourite,
-    Item
+    Item,
+    Move,
+    Nature
 }
