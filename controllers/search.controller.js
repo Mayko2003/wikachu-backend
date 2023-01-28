2e8033595bc4b0dedb5353cf0b0f4add2c8e2c70
Mayko2003 <maykox34f@gmail.com>
2023-01-28 20:03:07 -0300

    chore: add new results for searching

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -1,4 +1,4 @@
-const { Pokemon } = require('../models')
+const { Pokemon, Item, Move, Nature } = require('../models')
 
 const SearchController = {}
 
@@ -8,9 +8,16 @@ SearchController.search = async(req, res) => {
     try {
         const q = req.query.q
         const pokemons = await Pokemon.find({ name: { $regex: q, $options: 'i' } })
+        const items = await Item.find({ name: { $regex: q, $options: 'i' } })
+        const moves = await Move.find({ name: { $regex: q, $options: 'i' } })
+        const natures = await Nature.find({ name: { $regex: q, $options: 'i' } })
+
         res.status(200).json({
             data: {
-                pokemons
+                pokemons,
+                items,
+                moves,
+                natures
             }
         })
     } catch (error) {
