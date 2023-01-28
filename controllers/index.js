140cb1732df045b38f302b95561c368d7fdce04d
Mayko2003 <maykox34f@gmail.com>
2023-01-28 17:33:47 -0300

    chore: add item controller to exports

--- controllers/index.js
+++ controllers/index.js
@@ -4,11 +4,13 @@ const userController = require('./user.controller');
 const authController = require('./auth.controller');
 const favouriteController = require('./favourite.controller');
 const searchController = require('./search.controller');
+const itemController = require('./item.controller')
 
 module.exports = {
     pokemonController,
     userController,
     authController,
     favouriteController,
-    searchController
+    searchController,
+    itemController
 }
