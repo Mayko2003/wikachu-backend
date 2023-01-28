0a0cf0614f19e55dda81ef0c68b840c8d20f18e4
Mayko2003 <maykox34f@gmail.com>
2023-01-28 17:22:48 -0300

    chore: add search controller to exports

--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,12 @@ const pokemonController = require('./pokemon.controller');
 const userController = require('./user.controller');
 const authController = require('./auth.controller');
 const favouriteController = require('./favourite.controller');
+const searchController = require('./search.controller');
 
 module.exports = {
     pokemonController,
     userController,
     authController,
-    favouriteController
+    favouriteController,
+    searchController
 }
