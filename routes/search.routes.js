106a97f641d5ec6c51c222d12af6d3b83723a34a
Mayko2003 <maykox34f@gmail.com>
2023-01-28 17:23:07 -0300

    chore: add search routes

---
+++ routes/search.routes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+const { searchController } = require('../controllers')
+
+
+router.get('/', searchController.search)
+
+module.exports = router
